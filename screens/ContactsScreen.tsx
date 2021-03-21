d3e7301a46ff0d569efd037924017c3fa2e658f4
Ignacio García Hernández <ignaciogarciahernandez@MacBook-Pro-de-Ignacio.local>
2021-03-21 16:25:29 +0100

    display users API

--- screens/ContactsScreen.tsx
+++ screens/ContactsScreen.tsx
@@ -7,10 +7,27 @@ import ChatListItem from '../components/ChatListItem';
 import users from '../data/Users'
 import { FlatList } from 'react-native-gesture-handler';
 import ContactListItem from '../components/ContactListItem';
+import { useEffect, useState } from 'react';
+import { API, graphqlOperation } from 'aws-amplify';
+import { listUsers } from '../src/graphql/queries';
 
 
 
 export default function ContactsScreen() {
+  const [users, setUsers] = useState([])
+
+  useEffect(()=>{
+    const fetchUsers = async () =>{
+      try {
+        const usersData = await API.graphql(graphqlOperation(listUsers))
+        setUsers(usersData.data.listUsers.items);
+      } catch (error) {
+        console.log(error)
+      }
+      
+    }
+    fetchUsers();
+  },[])
   return (
     <View style={styles.container}>
       <FlatList 
